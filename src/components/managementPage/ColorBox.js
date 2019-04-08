a2192636be943b6c8230f99ee3dc6e59413bc0da
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-08 12:58:22 +0900

    popup ok

--- src/components/managementPage/ColorBox.js
+++ src/components/managementPage/ColorBox.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 import ColorBoxContent from './ColorBoxContent'
+import classes from '../../assets/managementPage/ColorBox.css'
 
+class Popup extends React.Component {
+    render() {
+      return (
+        <div className={classes.popup}>
+          <div className={classes.popup_inner}>
+            <h1>{this.props.text}</h1>
+          <button onClick={this.props.closePopup}>close me</button>
+          </div>
+        </div>
+      );
+    }
+}
 class ColorBox extends Component {
+    constructor() {
+        super();
+        this.state = {
+          showPopup: false
+        };
+    }
+    togglePopup() {
+        this.setState({
+          showPopup: !this.state.showPopup
+        });
+    }
+
     render(){
         return(
             <div>
                 Color box
+                <div className='colorBox'>
+                    <h1>hihi</h1>
+                    <button onClick={this.togglePopup.bind(this)}>show popup</button>
+                    <button onClick={() => {alert('hahha?');}}>try me when popup is open</button>
+                    <p>Ganz viel inhalt</p>
+                    {this.state.showPopup ? 
+                        <Popup
+                        text='Close Me'
+                        closePopup={this.togglePopup.bind(this)}
+                        />
+                        : null
+                    }
+                </div>
                 <ColorBoxContent />
             </div>
         )
@@ -13,3 +51,4 @@ class ColorBox extends Component {
 }
 
 export default ColorBox;
+
